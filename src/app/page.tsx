051ddc5b082496aa29cb4b5f19a3ef1c90d6723a
ryuno3 <n.ryuno3@gmail.com>
2024-12-29 00:36:11 +0900

    ✨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
+import Hero from "@/components/sections/Hero/Hero";
 
 export default function Home() {
   return (
     <div className="">
-
+      <Header />
+      <main className="p-0 m-0">
+        <Hero />
+      </main>
+      <Footer />
     </div>
   );
 }
