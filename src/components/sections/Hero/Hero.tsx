b10124cd24f514cb75508f24f8c3e9228c67bd4d
ryuno3 <n.ryuno3@gmail.com>
2025-01-06 23:55:28 +0900

    💄 レスポンシブ強化

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -3,10 +3,10 @@ import HeroAnimation from "@/components/sections/Hero/HeroAnimation";
 
 export default function Hero() {
   return (
-    <div className="relative p-0 m-0 justify-center items-center h-screen">
+    <div className="relative p-0 m-0 justify-center items-center h-screen overflow-hidden w-[100%]">
       <video
         src="/hero_back.mp4"
-        className="absolute inset-0 z-0 object-cover min-w-screen min-h-screen"
+        className="absolute inset-0 z-0 object-cover min-h-screen min-w-full"
         autoPlay
         loop
         muted
@@ -15,7 +15,7 @@ export default function Hero() {
       <div
         className="absolute left-1/2 top-[45%] -translate-x-1/2 -translate-y-1/2 z-10 
         text-2xl sm:text-3xl md:text-4xl lg:text-4xl font-bold font-maru text-white
-        p-4 sm:p-6 md:p-8 lg:p-10 w-full sm:w-auto"
+        p-4 sm:p-6 md:p-8 lg:p-10 w-full "
       >
         <HeroAnimation />
       </div>
