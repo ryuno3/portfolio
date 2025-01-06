bbc52b7a4904ae89bd7c069b362d5b5bd1e80fae
ryuno3 <n.ryuno3@gmail.com>
2025-01-07 00:46:20 +0900

    💄 矢印はこれで。Imageコンポーネントで出てた警告の修正

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,25 +1,36 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { useScrollDirection } from "@/hooks/useScrollDirection";
 
 const Header = () => {
+  const scrollDirection = useScrollDirection();
+
   const menu = [
     { name: "Home", href: "/" },
     { name: "About", href: "/about" },
     { name: "Works", href: "/works" },
     { name: "Contact", href: "/contact" },
   ];
+
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 flex justify-between items-center p-5 bg-transparent">
-      <nav>
-        <ul className="flex gap-4 lg:gap-8">
-          {menu.map((item) => (
-            <li key={item.name} className="font-bold lg:text-3xl">
-              <Link href={item.href}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+    <header
+      className={`fixed top-0 left-0 right-0 z-50 transition-transform duration-300
+      ${scrollDirection === "down" ? "-translate-y-full" : "translate-y-0"}`}
+    >
+      <div className="flex justify-between items-center p-2 backdrop-blur-sm">
+        <nav>
+          <ul className="flex gap-4 lg:gap-8">
+            {menu.map((item) => (
+              <li key={item.name} className="font-bold lg:text-3xl">
+                <Link href={item.href}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
