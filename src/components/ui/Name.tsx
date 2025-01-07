4e1b8e7422fd91af74e6351f8e4f2ef927809ac7
ryuno3 <n.ryuno3@gmail.com>
2025-01-08 01:36:25 +0900

    💄 名前分かりづらかったから蛍光ペン風

--- src/components/ui/Name.tsx
+++ src/components/ui/Name.tsx
@@ -5,9 +5,13 @@ import React from "react";
 const Name = () => {
   return (
     <h1 className="text-4xl m-5 font-maru">
-      <span className="md:inline block">西野 龍ノ介</span>
-      <span className="md:inline md:before:content-['/'] md:before:mx-2">
+      <span className="md:inline block relative">
+        西野 龍ノ介
+        <span className="absolute bottom-1 left-0 w-full h-[10px] bg-yellow-200/30 -z-10"></span>
+      </span>
+      <span className="md:inline md:before:content-['/'] md:before:mx-2 relative">
         Nishino Ryunosuke
+        <span className="absolute bottom-1 left-0 w-full h-[10px] bg-yellow-200/30 -z-10"></span>
       </span>
     </h1>
   );
