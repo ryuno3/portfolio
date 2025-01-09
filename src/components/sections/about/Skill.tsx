73d1cec5ce4c60400c3c771b2626d8991570f53d
ryuno3 <n.ryuno3@gmail.com>
2025-01-10 01:06:01 +0900

    🐛 ばか

--- src/components/sections/about/Skill.tsx
+++ src/components/sections/about/Skill.tsx
@@ -2,19 +2,13 @@ import Headline from "@/components/ui/Headline";
 import React from "react";
 import Language from "@/components/ui/Language";
 import { FaReact } from "react-icons/fa";
-import {
-  SiTypescript,
-  SiNextdotjs,
-  SiFlutter,
-  SiSupabase,
-  SiPrisma,
-} from "react-icons/si";
+import { SiTypescript, SiNextdotjs, SiFlutter, SiSupabase, SiPrisma } from "react-icons/si";
 const Skill = () => {
   return (
     <div>
       <section className=" border-2 border-white rounded-lg p-2 pb-10">
         <header>
-          <Headline title="言語 | フレームワーク|ツール" />
+          <Headline title="言語 | フレームワーク|ツール" size="m" />
         </header>
         <article className="flex flex-col">
           <ul className="flex-1 md:flex flex-row  gap-2">
@@ -23,11 +17,7 @@ const Skill = () => {
                 <Language language="React" level={2} icon={<FaReact />} />
               </li>
               <li className="flex flex-row ">
-                <Language
-                  language="TypeScript"
-                  level={1}
-                  icon={<SiTypescript />}
-                />
+                <Language language="TypeScript" level={1} icon={<SiTypescript />} />
               </li>
               <li className="flex flex-row">
                 <Language language="Next.js" level={1} icon={<SiNextdotjs />} />
